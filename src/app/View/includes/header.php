c1cbf4c3579bb5f6132718121fa617960a4a0394
Lucas Ielli <lucas.ielli.pro@gmail.com>
2021-09-06 17:09:23 +0200

    Worked on DB, MVC implanted

---
+++ src/app/View/includes/header.php
@@ -0,0 +1,10 @@
+<header>
+    <ul>
+        <li>
+            <a href="index.php">Home</a>
+        </li>
+        <li>
+            <a href="index.php?page=articles-index">Articles</a>
+        </li>
+    </ul>
+</header>
