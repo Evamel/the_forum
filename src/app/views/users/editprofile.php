15859bab38d3bdcd54a6e46b73872c8ac563cc08
SporoZoite <lisavanhooydonk@gmail.com>
2021-09-10 15:46:30 +0200

    update user profile

--- src/app/views/users/editprofile.php
+++ src/app/views/users/editprofile.php
@@ -3,36 +3,36 @@ require APPROOT . '/views/includes/head.php';
 ?>
 
 <div id="section-landing">
-   
+<?php 
+var_dump($_SESSION);  
+?> 
 <?php
 require APPROOT . '/views/includes/navigation.php';
 ?>
-<?php 
-var_dump($_SESSION);  
-?>
+
 <div class="container-login">
    <div class="wrapper-login">
         <h2>Your Profile</h2>
            <form action="<?php  echo URLROOT; ?>/users/editprofile" method="POST">
-               <input type="text" placeholder="<?php $_SESSION['username'] ?>" name="username">
-               <span class="invalidFeedback">
-                   <?php echo $data['emailError']; ?>
-               </span>
-
-               <input type="email" placeholder="Email *" name="email">
+            Your kitty name:
+               <input type="text" placeholder="<?php echo $_SESSION['username'] ?>" name="username">
                <span class="invalidFeedback">
                    <?php echo $data['usernameError']; ?>
                </span>
-
-               <input type="password" placeholder="Password *" name="confirmPassword">
+               <br>
+            Your email :
+               <input type="email" placeholder="<?php echo $_SESSION['email'] ?>" name="email">
                <span class="invalidFeedback">
-                   <?php echo $data['confirmPasswordError']; ?>
+                   <?php echo $data['emailError']; ?>
                </span>
-
-               <input type="password" placeholder="Confirm password *" name="password">
+               <br>
+            Your signature :
+               <br>
+               <textarea rows="2" cols="80" placeholder="<?php echo $_SESSION['signature'] ?>" name="signature"></textarea>
                <span class="invalidFeedback">
-                   <?php echo $data['passwordError']; ?>
+                   <?php echo $data['signatureError']; ?>
                </span>
+               <br>
 
                <button id="submit" type="submit" value="submit">
                 Edit Profile
