a8d3b7e2ccce252fd0eb2bdd23ccd9d542e4df78
SporoZoite <lisavanhooydonk@gmail.com>
2021-09-14 12:21:43 +0200

    all pages ok, no linking

--- src/app/controllers/Boards.php
+++ src/app/controllers/Boards.php
@@ -1,3 +1,28 @@
- $sumTopics = $this->topicModel->sumTopics();
- $sumPosts = $this->topicModel->sumPosts();
- $lastPost = $this->topicModel->lastPost();
+<?php
+class Boards extends Controller{
+    public function __construct(){
+       $this->boardModel = $this->model('Board'); 
+    }
+
+
+    public function index() {
+        $boards = $this->boardModel->getBoards();
+        $sumTopics = $this->boardModel->sumTopics();
+        $sumPosts = $this->boardModel->sumPosts();
+        $lastPost = $this->boardModel->lastPost();
+       
+   
+
+        $data = [
+          'boards' => $boards,
+          'sumTopics' =>$sumTopics,
+          'sumPosts' =>$sumPosts,
+          'lastPost' =>$lastPost
+        ];
+   
+
+        $this->view('boards/index', $data);
+    }
+
+
+}
--- src/app/models/Board.php
+++ src/app/models/Board.php
@@ -15,21 +15,21 @@ class Board
 
 
     public function sumTopics(){
-        $this->db->query('SELECT * FROM topics ORDER BY topic_date ASC');
+        $this->db->query('SELECT COUNT(topic_id) AS topicTotal,topics.board_id,boards.board_id,boards.board_name FROM topics RIGHT OUTER JOIN boards ON topics.board_id = boards.board_id GROUP BY board_name;');
         $results = $this->db->resultSet();
       //query to display user name and signature
         return $results;
      }
 
      public function sumPosts(){
-        $this->db->query('SELECT * FROM topics ORDER BY topic_date ASC');
+        $this->db->query('SELECT COUNT(message_id) AS messageTotal,boards.board_id,boards.board_name FROM messages RIGHT OUTER JOIN topics ON messages.topic_id = topics.topic_id INNER JOIN boards on topics.board_id = boards.board_id GROUP BY board_name ORDER BY board_id ASC;');
         $results = $this->db->resultSet();
       //query to display user name and signature
         return $results;
      }
 
      public function lastPost(){
-        $this->db->query('SELECT * FROM topics ORDER BY topic_date ASC');
+        $this->db->query('SELECT * FROM boards LEFT OUTER JOIN topics ON boards.board_id = topics.board_id ORDER BY topic_date DESC LIMIT 3;');
         $results = $this->db->resultSet();
       //query to display user name and signature
         return $results;
