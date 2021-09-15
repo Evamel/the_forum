1f319784c1e0d14653ec9ec8f2ce3d6e3fca3931
SporoZoite <lisavanhooydonk@gmail.com>
2021-09-15 09:32:57 +0200

    add content in pages of footbar

--- src/app/views/pages/about.php
+++ src/app/views/pages/about.php
@@ -1,5 +1,35 @@
- <?php
-    //members// nous grands créateurs du site
+<?php
+require APPROOT . '/views/includes/head.php';
+?>
+<div id="section-landing">
+ 
+<?php 
+var_dump($_SESSION);  
+?>
+<?php 
+var_dump($_GET['$id']);  
+?>
+
+<?php
+require APPROOT . '/views/includes/navigation.php';
+?>
+
+<?php
+require APPROOT . '/views/includes/right.php';
+?>
+
+
+<div class ="container">
+
+<?php
+   //nous grands créateurs du site
     echo 'About us'
     ?>
-    
+  
+</div>
+</div>
+
+<?php
+require APPROOT . '/views/includes/footer.php';
+?>
+
---
+++ src/app/views/pages/terms.php
@@ -0,0 +1,36 @@
+<?php
+require APPROOT . '/views/includes/head.php';
+?>
+
+<div id="section-landing">
+ 
+<?php 
+var_dump($_SESSION);  
+?>
+<?php 
+var_dump($_GET['$id']);  
+?>
+
+<?php
+require APPROOT . '/views/includes/navigation.php';
+?>
+
+<?php
+require APPROOT . '/views/includes/right.php';
+?>
+
+
+<div class ="container">
+
+<?php
+
+    echo 'Terms'
+    ?>
+  
+</div>
+</div>
+
+<?php
+require APPROOT . '/views/includes/footer.php';
+?>
+
